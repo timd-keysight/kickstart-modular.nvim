600f79ef5983a70d416865af31c923932e69f37e
Steven Coenen <steven.coenen@keysight.com>
2024-10-03 18:42:10 +0200

    Add mini.move to move lines

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -17,6 +17,9 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- Move lines up and down (and left and right) with alt+hjkl
+      require('mini.move').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
